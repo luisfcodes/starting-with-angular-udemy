0d5eb0f4e98a15e81a9fd601712e342456e0da00
Luis Fernando <luis.silva9902@gmail.com>
2022-10-21 17:42:48 -0300

    data binding in Angular

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core'
 })
 
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'Offline'
 
+  getServerStatus(){
+    return this.serverStatus
+  }
 }
